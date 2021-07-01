43c5a31b92976eb9d9aee6a13969548c8e3cdb39
Manas Pant <Manas.Pant@kantar.com>
2021-07-01 21:06:45 +0530

    Entity Framework

---
+++ EntityFramework/EntityFrameWorkInDal/Model/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EntityFrameWorkInDal.Model
+{
+    [Table("Students")]
+    public class Student
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Column(TypeName ="varchar(200)")]
+        [Required]
+        public string Name { get; set; }
+        public int Marks { get; set; }
+        [Column(TypeName = "varchar(250)")]
+        public string Subjects { get; set; }
+    }
+}
